8594986d5cc1b89d4beddde02f7e6b395c97c465
bernard kones <kngbudda@gmail.com>
2018-01-07 03:59:17 +0100

    Add files via upload

---
+++ one.js
@@ -0,0 +1,57 @@
+
+
+
+     var txt = "responive_01.04.01";
+     document.write("<p>Link: " + txt.link(
+       "https://docs.google.com/document/d/1Alha6XUTDw3IlfF6xIOwLXMF0ZUQEhIdbzMSqXOkigM/edit?usp=sharing"
+     ) + "</p>");
+   </script>
+   <script>
+     var txt = "responive_01.04.01";
+     document.write("<p>Link: " + txt.link(
+       "https://docs.google.com/document/d/1Alha6XUTDw3IlfF6xIOwLXMF0ZUQEhIdbzMSqXOkigM/edit?usp=sharing"
+     ) + "</p>");
+   </script>
+   <script>
+     var txt = "responive_01.04.01";
+     document.write("<p>Link: " + txt.link(
+       "https://docs.google.com/document/d/1Alha6XUTDw3IlfF6xIOwLXMF0ZUQEhIdbzMSqXOkigM/edit?usp=sharing"
+     ) + "</p>");
+   </script>
+   <h3> animation</h3>
+   <script>
+     var txt = "responive_01.04.01";
+     document.write("<p>Link: " + txt.link(
+       "https://docs.google.com/document/d/1Alha6XUTDw3IlfF6xIOwLXMF0ZUQEhIdbzMSqXOkigM/edit?usp=sharing"
+     ) + "</p>");
+   </script>
+   <script>
+     var txt = "responive_01.04.01";
+     document.write("<p>Link: " + txt.link(
+       "https://docs.google.com/document/d/1Alha6XUTDw3IlfF6xIOwLXMF0ZUQEhIdbzMSqXOkigM/edit?usp=sharing"
+     ) + "</p>");
+   </script>
+   <script>
+     var txt = "responive_01.04.01";
+     document.write("<p>Link: " + txt.link(
+       "https://docs.google.com/document/d/1Alha6XUTDw3IlfF6xIOwLXMF0ZUQEhIdbzMSqXOkigM/edit?usp=sharing"
+     ) + "</p>");
+   </script>
+   <script>
+     var txt = "responive_01.04.01";
+     document.write("<p>Link: " + txt.link(
+       "https://docs.google.com/document/d/1Alha6XUTDw3IlfF6xIOwLXMF0ZUQEhIdbzMSqXOkigM/edit?usp=sharing"
+     ) + "</p>");
+   </script>
+   <script>
+     var txt = "responive_01.04.01";
+     document.write("<p>Link: " + txt.link(
+       "https://docs.google.com/document/d/1Alha6XUTDw3IlfF6xIOwLXMF0ZUQEhIdbzMSqXOkigM/edit?usp=sharing"
+     ) + "</p>");
+   </script>
+   <script>
+     var txt = "responive_01.04.01";
+     document.write("<p>Link: " + txt.link(
+       "https://docs.google.com/document/d/1Alha6XUTDw3IlfF6xIOwLXMF0ZUQEhIdbzMSqXOkigM/edit?usp=sharing"
+     ) + "</p>");
+   </script>
